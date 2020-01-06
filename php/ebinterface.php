8c04c430434b58d5aa710ac5f80b5e3840b1c7d1
Ian Shepard <ianshepard@gmail.com>
2020-01-06 15:24:53 -0500

    + implement getAllEvents() + implement getAllAttendees()

--- php/ebinterface.php
+++ php/ebinterface.php
@@ -1,31 +1,88 @@
 <?php
-$api_key = "2GEOZ7XABZBZWS46A3";
-$pri_token = "COKR3D7YQAPZM2GWLOTL";
-$pub_token = "UKQY46DJTOTSARNGUAWH";
-$event_ID = "86470394277";
+$API_KEY = "2GEOZ7XABZBZWS46A3";
+$PRI_TOKEN = "COKR3D7YQAPZM2GWLOTL";
+$PUB_TOKEN = "UKQY46DJTOTSARNGUAWH";
+$EVENT_ID = "86470394277";
 //header("Authorization: Bearer ".$pri_token);
 
-function retrieveData($url) {
-	echo 	"<u>".$url."</u><br>";
-	$contents = file_get_contents($url);
-	echo $contents."<br><br>";
-	return $contents;
-}
-
-function wrapRequest($request, $args) {
+function wrapRequestWtihData($request, $data) {
 	static $endpoint = "https://www.eventbriteapi.com/v3/";
 	static $my_token = "?token=COKR3D7YQAPZM2GWLOTL";
+
+	$args = "";
+	foreach ($data as $arg) {
+		$args += "?".$arg;
+	}
+
 	return $endpoint.$request.$my_token.$args;
 }
 
-$my_events_url = wrapRequest("users/me/events", "");						//get all events' details
-$event_list_url = wrapRequest("events/".$event_ID, ""); 					//get one event's details
-$attendee_list_url = wrapRequest("events/".$event_ID."/attendees/", "");	//get the attendee list for a spesific event
+function wrapRequest($request) {
+	return wrapRequestWtihData($request, []);
+}
 
-retrieveData($my_events_url);
-retrieveData($event_list_url);
-retrieveData($attendee_list_url);
+function getJsonFromURL($url) {
+	return json_decode(file_get_contents($url), true);
+}
 
+function getAllEvents($OAuthToken) {
+	$contents = getJsonFromURL(wrapRequest("users/me/events"));
+	$events = $contents["events"];
+	
+	$event_list = [];
+
+	$event_num = 0;
+	foreach ($events as $event) {
+		$event_list[$event_num] = array("name"=>$event["name"]["text"], 
+										"id"=>$event["id"], 
+										"event_url"=>$event["url"], 
+										"ticket_url"=>$event["resource_uri"]
+										);
+		$event_num++;
+	}
+	return $event_list;
+}
+
+function getAllAttendees($event_id) {
+	$contents = getJsonFromURL(wrapRequest("events/".$event_id."/attendees/"));
+	$attendees = $contents["attendees"];
+	//add pagination
+	return $attendees;
+}
+
+
+$my_events_url = wrapRequest("users/me/events");						//get all events' details
+$event_list_url = wrapRequest("events/".$EVENT_ID); 					//get one event's details
+$attendee_list_url = wrapRequest("events/".$EVENT_ID."/attendees/");	//get the attendee list for a spesific event
+
+
+
+echo $attendee_list_url;
+echo "<br>";
+$events = getAllEvents($PRI_TOKEN);
+
+$attendees = getAllAttendees($events[0]["id"]);
+$attendee = $attendees[0];
+echo $attendee["profile"]["name"];
+echo "<br>";
+echo $attendee["id"];
+echo "<br>";	
+
+
+
+
+
+
+
+function testGetAllEvents() {
+	$events = getAllEvents($PRI_TOKEN);
+	echo $events[0]["name"];
+	echo "<br>";
+	echo $events[0]["id"];
+	echo "<br>";
+	echo $events[0]["event_url"];
+	echo "<br>";
+}
 ?>
 
 
@@ -34,6 +91,9 @@ retrieveData($attendee_list_url);
 	<body>
 		<a href="../ui/setup.php">Setup page</a>
 		<br>
+		getAllEvents($OAuthToken) //Pri_token get all of the events of a user<br>
+		getAttendees($event_id) // get a list of all attendees' full name and email "fullname": "email" or 0 = {"name": <\fullname>, "email": <\email>}<br>
+		getAttendeeInfo($attendee_id) // return entire attendee object<br>
 	</body>
 
 </html>
