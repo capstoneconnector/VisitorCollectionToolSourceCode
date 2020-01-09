9365ff4d6d6f3ea0e02c2a0f1209eb84f957a81f
Kylie Harmon <kharmon3@bsu.edu>
2020-01-09 11:47:57 -0500

    Update Analytics and clean up code

--- ui/Analytics.php
+++ ui/Analytics.php
@@ -3,18 +3,36 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src = "/js/manager.js"></script>
 		<link rel = "stylesheet" type = "text/css" href = "/css/manager.css">
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<title>Manager Dashboard</title>
 	</head>
 	<body>
-	<div id = "main">
-		<div id ="menu">
-		<ul>
-	<li><a href='#'><span>Set Up</span></a></li>
-	<li><a href='Analytics.php'><span>Analytics</span></a></li>
-	<li class='last'><a href='manager.php'><span>Events</span></a></li>
-	<li 
-</ul>
+	<div class = "container">
+	<div class = "row">
+		<div class="col-2">
+			<img src="/img/Innovation Connector Logo.png" width="150px"></img>
+			<div id ="menu">
+				<ul>
+					<li><a href='setup.php'><span>Set Up</span></a></li>
+					<li><a href='Analytics.php'<span>Analytics</span></a></li>
+					<li class='last'><a href='manager.php'><span>Events</span></a></li>
+				</ul>
+			</div>
 		</div>
-	</div>
+			<div class="col-10">
+			<table width = "100%" style = "background:#05163D; color: honeydew" align="right">
+				<tr>
+					<td width = "20">&nbsp;</td>
+					<td>
+						<h2>Analytics</h2>
+					</td>
+					<td>&nbsp;</td>
+			</div>
+		</div>
+		</div>
 	</body>
 </html>
