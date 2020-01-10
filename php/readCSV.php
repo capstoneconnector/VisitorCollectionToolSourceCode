34ed8b48b34cbf5f06fd79c04a7928b1af015b09
mdnessel <Shogun32498>
2020-01-10 13:36:01 -0500

    Delete legacy readCSV.php script

--- php/readCSV.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function readCSV($file){
-		$info = array();
-		while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
-			array_push($info, $data); //Put each row of the csv into an array
-		}
-		return $info;
-	}
-?>
