fcfdc554c89dd1bbff94b6ddce325469282a2e0f
Matthew <mdnessel24@gmail.com>
2020-01-16 13:22:24 -0500

    Modify addAttendee to check in user on registration automatically

--- db/addAttendee.php
+++ db/addAttendee.php
@@ -1,7 +1,7 @@
 <?php
 	function registerUser($fname, $lname, $email, $event){
 		$pdo = new PDO('mysql:host=localhost;dbname=icdb', "root", "");
-		$stmt = $pdo->prepare("INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES(?,?,?,?,FALSE)");
+		$stmt = $pdo->prepare("INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES(?,?,?,?,TRUE)");
 		$stmt->bindParam(1,$fname);
 		$stmt->bindParam(2,$lname);
 		$stmt->bindParam(3,$email);
