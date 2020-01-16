f2eeb444607c7db555a1ad6bb848750d91994810
Matthew <mdnessel24@gmail.com>
2020-01-15 21:27:40 -0500

    Add tests for getEventInfo.php

---
+++ tests/getEventInfoTest.php
@@ -0,0 +1,16 @@
+<?php
+	include_once "../db/getEventInfo.php";
+
+	class GetEventInfo extends PHPUnit_Framework_TestCase{ //Run this file with phpunit command from command line
+		public function setUp(){
+			$this->eventid = 999;
+		}
+		public function tearDown(){
+		}
+
+		function testGetEventByIdSuccess(){
+			$this->assertSame(getEventById($this->eventid)['Name'], "Test Event");
+		}
+
+	}
+?>
