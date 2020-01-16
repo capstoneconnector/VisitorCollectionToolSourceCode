324d67427a444dee9e43cd529f90baca37ca3b27
Matthew <mdnessel24@gmail.com>
2020-01-15 21:14:41 -0500

    Add tests for getAttendeeInfo

---
+++ tests/getAttendeeInfoTest.php
@@ -0,0 +1,19 @@
+<?php
+	include_once "../db/getAttendeeInfo.php";
+
+	class AddEventTest extends PHPUnit_Framework_TestCase{ //Run this file with phpunit command from command line
+		public function setUp(){
+			$this->eventid = 999;
+		}
+		public function tearDown(){
+		}
+
+		function testGetAttendeeCountOneResult(){
+			$this->assertSame(getAttendeeCount("Bob", "Jones", "bjones@gmail.com", $this->eventid)['num'], "1");
+		}
+
+		function testGetAttendeeCountNoResult(){
+			$this->assertSame(getAttendeeCount("Tommy", "Cat", "tommythecat@gmail.com", $this->eventid)['num'], "0");
+		}
+	}
+?>
