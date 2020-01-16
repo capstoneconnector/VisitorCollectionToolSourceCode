aa04077fa8bf30302cff16517bc60c12a5a41893
Matthew <mdnessel24@gmail.com>
2020-01-15 21:17:14 -0500

    Change test class name

--- tests/getAttendeeInfoTest.php
+++ tests/getAttendeeInfoTest.php
@@ -1,7 +1,7 @@
 <?php
 	include_once "../db/getAttendeeInfo.php";
 
-	class AddEventTest extends PHPUnit_Framework_TestCase{ //Run this file with phpunit command from command line
+	class GetAttendeeInfoTest extends PHPUnit_Framework_TestCase{ //Run this file with phpunit command from command line
 		public function setUp(){
 			$this->eventid = 999;
 		}
