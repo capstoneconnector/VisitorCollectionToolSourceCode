969a17397d405611438ab3faeac917e23dd8cbe7
Kylie Harmon <kharmon3@bsu.edu>
2020-01-25 19:01:52 -0500

    update styling

--- ui/checkin.php
+++ ui/checkin.php
@@ -8,66 +8,65 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="/js/checkin.js"></script>
 		<link rel="stylesheet" type="text/css" href="/css/checkin.css">
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- Popper.JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<!-- Bootstrap JS -->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
 	<body>
 		<div class="container">
 			<div class="row">
 				<div class="col-2">
 				<title>Check In</title>
 	</head>
-					<div class="panel-group" id="menu">
-						<div class="panel panel-default">
-							<div class="panel-heading">
-							<h4 class="panel-title">
-							<a data-toggle="collapse" href="#collapse1"></a>
-							</h4>
-							</div>
-							<ul>
-						<div id="collapse1" class="panel-collapse collapse">
-						<div class="panel-body"><li><a href='setup.php'><span>Set Up</span></a></li></div>
-						<div class="panel-footer"><li class='last'><a href='register.php'><span>Registration</span></a></li></div>
-						</ul>
-						</div>
-						</div>
-					</div>
-					</div>
 				</div>
+			</div>
 				<div id="main">
 					<div class="col-10">
 						<table width = "100%" style = "background:#05163D; color: honeydew" align="right">
 							<tr>
-								<td width = "20">&nbsp;</td>
-								<td>
-								<h2>Check In</h2>
-								</td>
 								<td>&nbsp;</td>
-								<td width = "10">&nbsp;</td>
-							</tr>
-							<tr>
-								<td colspan "2"></td>
+								<td>
+									<div class="container mt-1" id="menu">
+									<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#content">  </button>
+										<div id="content" class="collapse">
+											<li><a href='setup.php'><span>setup</span></a></li>
+											<li class='last'><a href='register.php'><span>Registration</span></a></li>
+										</div>
+									</div>
+								</td>
+								<td>
+									<h2>Check In</h2>
+								</td>
+								<td width = "45">&nbsp;</td>
 							</tr>
 						</table>
-			<?php
-				$event = getEventById($_SESSION["eventId"]); //Use session var for event name
-				echo "<h2>" . $event["Name"] . "</h2>";
-			?>
-			<br>
-			<form method="post">
-				<span id="prompt">Enter your name</span>
-				<br><br>
-				<input class="input" type="text" name="name" required>
-				<br><br>
-				<input class="submit" type="submit" value="Check In">
-				<br><br>
-			</form>
-			<button class="submit" onclick="window.location = 'setup.php'"><img src="../img/home_icon.png" alt="home icon" height="32"></button>
-		</div>
-		</div>
-		<br><br><br>
-		<div class="table">
+						<?php
+							$event = getEventById($_SESSION["eventId"]); //Use session var for event name
+							echo "<h2>" . $event["Name"] . "</h2>";
+						?>
+						<br>
+						<form method="post">
+						<span id="prompt">Enter your name</span>
+						<br><br>
+						<input class="input" type="text" name="name" required>
+						<br><br>
+						<input class="submit" type="submit" value="Check In">
+						<br><br>
+						</form>
+						<button class="submit" onclick="window.location = 'setup.php'"><img src="../img/home_icon.png" alt="home icon" height="32"></button>
+					</div>
+				</div>
+			<br><br><br>
+			<div class="table">
 			<?php
 				$root = $_SERVER['DOCUMENT_ROOT'];
 				include_once $root . "/php/findName.php";
@@ -97,8 +96,7 @@
 					}
 				}
 			?>
-		</div>
-		</div>
+			</div>
 		</div>
 	</body>
 </html>
--- ui/register.php
+++ ui/register.php
@@ -6,10 +6,39 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel = "stylesheet" type = "text/css" href = "/css/register.css">
+
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<div class="container">
+			<div class="row">
+				<div class="col-2">
+				<title>Registration</title>
 	</head>
 	<body>
-		<div id = "main">
-			<h1>Register</h1>
+	</div>
+		</div>
+		<div id="main">
+					<div class="col-10">
+						<table width = "100%" style = "background:#05163D; color: honeydew" align="right">
+							<tr>
+								<td>&nbsp;</td>
+								<td>
+									<div class="container mt-1" id="menu">
+									<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#content">  </button>
+										<div id="content" class="collapse">
+											<li><a href='setup.php'><span>setup</span></a></li>
+											<li class='last'><a href='checkin.php'><span>Check In</span></a></li>
+										</div>
+									</div>
+								</td>
+								<td>
+									<h2>Registration</h2>
+								</td>
+								<td width = "45">&nbsp;</td>
+							</tr>
+						</table>
 			<form method = "post">
 				<span id = "field">First Name <input class = "input" type = "text" name = "fname" required value="<?php echo isset($_POST["fname"]) ? $_POST["fname"] : ''?>"></span>
 				<br><br>
@@ -22,6 +51,7 @@
 			<form method = "post" action = "/ui/checkin.php">
 				<button class = "submit">Back</button>
 			</form>
+		</div>
 		</div>
 	</body>
 </html>
