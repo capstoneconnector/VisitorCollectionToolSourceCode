33f3fd23dd5f8a73c9b2c667c30fde5e3ed69dde
mdnessel <Shogun32498>
2020-02-18 11:20:03 -0500

    Add first name retrieval unit test to attendeeTest.php

--- tests/classes/attendeeTest.php
+++ tests/classes/attendeeTest.php
@@ -1,14 +1,25 @@
 <?php
 require_once "../../php/classes/Attendee.php";
-use PHPUnit\Framework\TestCase;
 
 
-class attendeeTest extends TestCase
+class attendeeTest extends PHPUnit_Framework_TestCase
 {
-    function createNewAttendeeTest()
-    {
-        //$this->assertInstanceOf(Attendee::class, $attendee = new Attendee());
+    private $attendee;
 
-        $this->assertTrue(true);
+    public function setUp(){
+        $this->attendee = new Attendee();
+        $this->attendee->createNew("Bob", "Jones", "bjones@gmail.com", "555-5555");
+    }
+
+    public function tearDown(){
+
+    }
+    function testCreateNewAttendee()
+    {
+        $this->assertInstanceOf(Attendee::class, $attendee = new Attendee());
+    }
+
+    function testGetFname(){
+        $this->assertSame("Bob", $this->attendee->getFname());
     }
 }
