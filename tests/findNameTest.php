423a7dce9b8af42c5dd9ce605d1ee0ff588ed19f
mdnessel <mdnessel24@gmail.com>
2020-02-28 00:34:19 -0500

    Modify findName test

--- tests/findNameTest.php
+++ tests/findNameTest.php
@@ -2,26 +2,28 @@
 	include_once "../php/findName.php"; 
 
 	class FindNameTest extends PHPUnit_Framework_TestCase{ //Run this file with phpunit command from command line
-		public function setUp(){
-			$this->eventid = 999;
+		private $event;
+	    public function setUp(){
+            $this->event = new Event();
+            $this->event->createNew(999, "Test Event", "2030-04-01", "");
 		}
 		public function tearDown(){
 		}
 
 		function testFindNameOneResult(){
-			$this->assertSame(findName("John Smith", $this->eventid)[0]['Email'], "jsmith@gmail.com");
+			$this->assertSame(findName("John Smith", $this->event)[0]->getEmail(), "jsmith@gmail.com");
 	
 		}
 
 		function testFindNameMoreThanOneResult(){
-			$this->assertSame(findName("Bob Jones", $this->eventid)[1]['Email'], "bjones2@gmail.com");
+			$this->assertSame(findName("Bob Jones", $this->event)[1]->getEmail(), "bjones2@gmail.com");
 		}
 
 		function testFindNameNoResultsWhenAlreadyRegistered(){
-			$this->assertEmpty(findName("Mary Jane", $this->eventid));
+			$this->assertEmpty(findName("Mary Jane", $this->event));
 		}
 
 		function testFindNameNoResults(){
-			$this->assertEmpty(findName("John Doe", $this->eventid));
+			$this->assertEmpty(findName("John Doe", $this->event));
 		}
 	}
