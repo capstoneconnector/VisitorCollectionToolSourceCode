80a4e98b19b0087771a702b097ade3a3836c72e8
mdnessel <mdnessel24@gmail.com>
2020-02-27 22:35:43 -0500

    Add layer for improved analytics coupling

--- php/getAttendanceInfo.php
+++ php/getAttendanceInfo.php
@@ -2,9 +2,6 @@
 require_once "../db/classes/DbClass.php";
 require_once "classes/Attendance.php";
 
-function getAttendanceByEventId($eventid){
-    $attendance = new Attendance();
-    $dbAttendance = DbClass::getAttendanceByEventId($eventid);
-    $attendance->createNew($dbAttendance["Walk-in"], $dbAttendance["Registered"], $dbAttendance["Attended"]);
-    return $attendance;
+function getAttendanceProportion($eventid){
+    return DbClass::getAttendanceByEventId($eventid);
 }
